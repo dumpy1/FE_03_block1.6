b6012d9549a8db90189dbce9af5cedc229d43884
Evgent Chertov <echertov@gmail.com>
2020-05-17 23:10:44 +0300

    переписал скрипт для слайдеров, тоглеров и модалок

--- src/js/breakpointchecker.js
+++ src/js/breakpointchecker.js
@@ -1,61 +1,142 @@
 import Swiper from 'swiper';
 'use strict';
-    const breakpoint = window.matchMedia('(min-width: 767px)');
-    let mySwiper;
-    console.log('mySwiper: ', mySwiper);
-    let swiperWrapper = document.querySelector('.slider');
+let mySwiper;
+const breakpoint = window.matchMedia('(min-width: 767px)');
+let swiperContainer = document.querySelectorAll('.swiper-container');
+console.log('swiperContainer: ', swiperContainer);
 
-    let swiperItem = document.querySelectorAll('.slider-item');
+let swiperWrapper;
+let swiperItem;
+let swiperPagination;
 
-    const breakpointChecker = function () {
-      console.log('mySwiper в функции: ', mySwiper);
-        if (breakpoint.matches) {
-            if (mySwiper !== undefined) mySwiper.destroy(true, true);
-            swiperWrapper.classList.remove('swiper-wrapper');
-            for (var i =0; i < swiperItem.length; i++) {
-    swiperItem[i].classList.remove('swiper-slide');
-}
-            return;
-        } else if (!breakpoint.matches) {
-            return enableSwiper();
+
+
+const breakpointChecker = function () {
+  console.log('mySwiper в функции: ', mySwiper);
+  if (breakpoint.matches) {
+
+    if (mySwiper !== undefined)
+      for (let i = 0; i < swiperContainer.length; i++) {
+        mySwiper[i].destroy(true, true);
+        swiperWrapper = swiperContainer[i].children;
+        swiperWrapper[0].classList.remove('swiper-wrapper');
+        swiperItem = swiperWrapper[0].children;
+        for (var j = 0; j < swiperItem.length; j++) {
+          swiperItem[j].classList.remove('swiper-slide');
         }
-    };
-    const enableSwiper = function () {
-swiperWrapper.classList.add('swiper-wrapper');
+        swiperPagination = swiperContainer[i].querySelector('.swiper-pagination');
+        console.log('swiperPagination: ', swiperPagination);
 
-for (var i =0; i < swiperItem.length; i++) {
-    swiperItem[i].classList.add('swiper-slide');
+        swiperContainer[i].removeChild(swiperPagination);
+      }
+    return;
+  } else if (!breakpoint.matches) {
+    return enableSwiper();
+  }
+};
+
+const enableSwiper = function () {
+
+  console.log('swiperWrapper: ', swiperWrapper);
+  for (let i = 0; i < swiperContainer.length; i++) {
+    swiperWrapper = swiperContainer[i].children;
+    swiperWrapper[0].classList.add('swiper-wrapper');
+    swiperItem = swiperWrapper[0].children;
+    for (var j = 0; j < swiperItem.length; j++) {
+      swiperItem[j].classList.add('swiper-slide');
+    }
+    swiperPagination = document.createElement('div');
+    swiperPagination.classList.add('swiper-pagination');
+    swiperContainer[i].appendChild(swiperPagination);
+  }
+  mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+
+    },
+  });
+  console.log('mySwiper в функции: ', mySwiper.pagination);
+};
+
+breakpoint.addListener(breakpointChecker);
+breakpointChecker();
+
+
+// toggler
+let showToggle = document.querySelectorAll('.show-toggle');
+let hiddenItem = document.querySelectorAll('.hidden-area');
+console.log('hiddenItem: ', hiddenItem);
+
+for (let i = 0; i < hiddenItem.length; i++) {
+  showToggle[i].addEventListener('click', () => {
+
+    hiddenItem[i].classList.add('hidden-area--visible');
+
+    if (showToggle[i].textContent == 'Показать все') {
+      showToggle[i].textContent = 'Скрыть';
+      showToggle[i].classList.add('show-toggle--rotate');
+    } else {
+      hiddenItem[i].classList.remove('hidden-area--visible');
+      showToggle[i].textContent = 'Показать все';
+      showToggle[i].classList.remove('show-toggle--rotate');
+
+    }
+  });
 }
-            mySwiper = new Swiper('.swiper-container', {
-                    loop: true,
-                    slidesPerView: 'auto',
-                    spaceBetween: 30,
-                    pagination: {
-                        el: '.swiper-pagination',
-                        type: 'bullets',
-                        clickable: true,
-                        
-                    },
-                });
-                console.log('mySwiper в функции: ', mySwiper.pagination);
-            };
-            breakpoint.addListener(breakpointChecker);
-            breakpointChecker();
+
+//pop-ups open/close
+
+let burger = document.querySelector('.button--menu');
+let sideMenu = document.querySelector('.side-bar');
+let sideMenuClose = sideMenu.querySelector('.button--close');
+let warFog = document.querySelector('.modal-overlay');
+let modal = document.querySelectorAll('.modal');
+console.log('modal: ', modal);
 
 
-            // toggler
-            let showToggle = document.querySelector('.show-toggle');
-            let hiddenItem = document.querySelector('.brands__slider');
-            showToggle.addEventListener('click', () => {
+let callButton = document.querySelectorAll('.button--call');
 
-                hiddenItem.style.height = 100 + '%';
-                if (showToggle.textContent == 'Показать все') {
-                    showToggle.textContent = 'Скрыть';
-                    showToggle.classList.add('show-toggle--rotate');
-                } else {
-                    hiddenItem.style.height = 200 + 'px';
-                    showToggle.textContent = 'Показать все';
-                    showToggle.classList.remove('show-toggle--rotate');
+let chatButton = document.querySelectorAll('.button--chat');
 
-                }
-            })
+
+let callback = document.querySelector('.callback');
+
+let feedback = document.querySelector('.feedback');
+
+burger.addEventListener('click', () => {
+  event.preventDefault();
+  sideMenu.classList.add('side-bar--visible');
+  warFog.classList.add('side-bar--visible');
+});
+sideMenuClose.addEventListener('click', () => {
+  event.preventDefault();
+  sideMenu.classList.remove('side-bar--visible');
+  warFog.classList.remove('side-bar--visible');
+});
+for(let i =0; i < callButton.length; i++) {
+callButton[i].addEventListener('click', () => {
+  event.preventDefault();
+  callback.classList.add('modal--visible');
+  warFog.classList.add('side-bar--visible');
+});}
+for(let i =0; i < chatButton.length; i++) {
+chatButton[i].addEventListener('click', () => {
+  event.preventDefault();
+  feedback.classList.add('modal--visible');
+  warFog.classList.add('side-bar--visible');
+});}
+
+for(let i =0; i < modal.length; i++) {
+  let modalClose = modal[i].querySelector('.button--close');
+  modalClose.addEventListener('click', () => {
+    event.preventDefault();
+  modal[i].classList.remove('modal--visible');
+  warFog.classList.remove('side-bar--visible');
+  })
+
+}
